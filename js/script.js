3d2cfadcbb21ce37c019f12721461eec537246cf
depst0r <depstor90@gmail.com>
2023-05-08 12:48:35 +0300

    heart-active

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-window.addEventListener("DOMContentLoaded", function () {});
+window.addEventListener("DOMContentLoaded", function () {
+  const section = document.querySelector("section");
+  const btns = section.querySelectorAll("i");
+  const heart = document.querySelector(".fa-heart");
+
+  const like = (heart) => {
+    heart.addEventListener("click", () => {
+      heart.classList.toggle("active");
+    });
+  };
+  like(heart);
+  function playPauseBnts(btns) {}
+
+  playPauseBnts(btns);
+});
