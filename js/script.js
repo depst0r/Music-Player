4abf08b727829a6b982a59653be1290d4c12a556
depst0r <depstor90@gmail.com>
2023-05-08 17:03:39 +0300

    json-server-db

--- js/script.js
+++ js/script.js
@@ -9,30 +9,49 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   };
 
-  //   function animation(iterm) {
-  //     const start = Date.now();
-
-  //     const timer = setInterval(() => {
-  //         let timePassed =
-  //     })
-  //   }
-
-  function dataBaseMusic(data) {
-    fetch(data)
-      .then((dataJSON) => dataJSON.json())
-      .then((res) => console.log(res));
-  }
-
-  class MyMysic {
-    constructor(id, artistName, nameOfTheSong, img, duration) {
-      this.id = id;
+  class MySongs {
+    constructor(artistName, nameOfTheSong, img, duration, ...classes) {
       this.artistName = artistName;
       this.nameOfTheSong = nameOfTheSong;
       this.img = img;
-      this.duration = duration;
-      this.parent = document.querySelector(parentSelector);
+      // this.duration = duration;
+      this.parent = document.querySelector(".songs");
+      this.classes = classes;
+    }
+
+    render() {
+      const element = document.createElement("div");
+
+      if (this.classes.length === 0) {
+        this.classes = "icon3";
+        element.classList.add(this.classes);
+      } else {
+        this.classes.map((className) => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+      <p class="name">${this.nameOfTheSong}</p>
+      <i class="fa fa-play"></i>
+      `;
+      this.parent.append(element);
     }
   }
+
+  const getPlayList = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fets ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getPlayList("http://localhost:3000/music").then((data) => {
+    data.forEach(({ artistName, nameOfTheSong, img }) => {
+      new MySongs(artistName, nameOfTheSong, img, ".songs .song").render();
+    });
+  });
 
   like(heart);
   function playPauseBnts(btns) {}
